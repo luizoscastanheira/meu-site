7cac14dd159a4cd9a9b437dde7929e8483f0fe1e
Luiz Otávio - ADS <93355309+luizoscastanheira@users.noreply.github.com>
2024-10-15 16:47:40 -0300

    Atualizando

--- src/componentes/Topo/index.jsx
+++ src/componentes/Topo/index.jsx
@@ -1,14 +1,28 @@
 /**
  * Criando o componente Topo
  */
-
 import Link from "next/link";
+import estilos from "./Topo.module.css";
 
 export default function Topo(){
     return(
-        <header>
-            <h3>Topo da Página</h3>
-            <nav>Contato</nav>
+        <header className={estilos.container_header}>
+
+            <div className={estilos.container_topo}>
+                <h1>
+                    <code>&lt;h1&gt;Luiz Otávio&lt;/h1&gt;</code>
+                </h1>
+                <button>Menu</button>
+                <nav className={estilos.menu}>
+                    <ul>
+                        <li>Home</li>
+                        <li>Certificados</li>
+                        <li>Projetos</li>
+                        <li>Contatos</li>
+                    </ul>
+                </nav>
+            </div>
+            
         </header>
     );
 }
