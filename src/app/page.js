a7e8efc899354b6506b1ae9f2a69375385583480
Luiz Otávio - ADS <93355309+luizoscastanheira@users.noreply.github.com>
2024-10-20 18:46:16 -0300

    Update:Menu
    Atualização geral do menu.

--- src/app/page.js
+++ src/app/page.js
@@ -3,20 +3,64 @@
  */
 
 //Importações
-'use client'
+'use client';
 import {useState} from "react";
 import Image from "next/image";
 import estilos from "./page.module.css";
 import Menu from "@/componentes/Menu";
-import Topo from "@/componentes/Topo"
+
+import ImgSecao from "../../public/computadorcopy.jpg";
 
 export default function Home() {
+// Criando a variavel de estado
+const [menuVisivel, setMenuVisivel] = useState(false);
+
+// criando uma funçao para ativiar/destivar o menu
+function toggleMenu(){
+  setMenuVisivel(!menuVisivel);
+  console.log(menuVisivel)
+}
 
   return(
-    <main>
-      <Menu />
-      <h1>Página do Luiz Otávio</h1>
+  <>
+    
+    <header className={estilos.container_header}>
+        <div className={estilos.container_topo}>
+          <button className={estilos.botao_menu} onClick={toggleMenu}>{menuVisivel ? '||' : '|||'}</button>
+          <h1>
+            <code>&lt;h1&gt;  Luiz Otávio  &lt;/h1&gt;</code>
+          </h1>
+        </div>
+    </header>
+      {menuVisivel && <Menu />}
+
+    <main className={estilos.main_container}>
+      <h1>Analista de Sistemas e Programador Front-End</h1>
+
+      <section className={estilos.conteudo}>
+        <p>
+          Não é fácil falar de si mesmo sem parecer "amostradinho", mas posso dizer
+          que em boa parte da minha vida profissional fui autodidata, especialmente ao trabalhar com sistemas operacionais Windows 
+          e Linux.
+        </p>
+        <p>
+          Descidi aprender programação e junto fazer minha graduação em Análise e Desenvolvimento de
+          Sistemas, cursos aos quais tenho dedicado tempo desde o final de 2022.
+        </p>
+        <p>
+          Posso me apresentar como alguem dedicado, que gosta do que faz e que está em constante
+          aprendizado na busca de solucionar problemas.
+        </p>
+      </section>
+      
+      <Image src={ImgSecao} alt="Imagem da Sec" className={estilos.img_secao}/>
       
     </main>
+    <section id={estilos.secao_competencias}>
+     
+    </section>
+
+  </>
+  
   );
 }
--- src/componentes/Menu/index.jsx
+++ src/componentes/Menu/index.jsx
@@ -6,10 +6,12 @@ export default function Menu(){
     return(
         <nav className={estilos.nav_container}>
             <ul className={estilos.menu}>
-                <li className={estilos.lista}><a href="#competencias">Competências</a></li>
-                <li className={estilos.lista}><a href="#projetos">Projetos</a></li>
-                <li className={estilos.lista}><a href="#contatos">Contato</a></li>
-                <li className={estilos.lista}><a href="#sobre">Sobre</a></li>
+                <li className={estilos.lista}><a href="#">Conheça-me</a></li>
+                <li className={estilos.lista}><a href="#">Competências</a></li>
+                <li className={estilos.lista}><a href="#">Projetos</a></li>
+                <li className={estilos.lista}><a href="#">LinkedIn</a></li>
+                <li className={estilos.lista}><a href="#">GitHub</a></li>
+                <li className={estilos.lista}><a href="#sobre">Sobre...</a></li>
             </ul>
         </nav>
     );
