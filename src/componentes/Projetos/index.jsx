b93a8a869e07cd86b6475888954c378cdb38faf2
Luiz Otávio - ADS <93355309+luizoscastanheira@users.noreply.github.com>
2024-10-28 20:22:56 -0300

    Up: Menu e Links

--- src/componentes/Projetos/index.jsx
+++ src/componentes/Projetos/index.jsx
@@ -12,20 +12,20 @@ export default function Projetos(){
         <section className={estilos.secao_projetos} id="projetos">
             <h2 className={estilos.titulo_secao}>Alguns dos meus projetos - ainda em construção!!!</h2>
         <div className={estilos.card_container}>
-           
+           <a href="">
                 <Card 
                     imagem = {Padaria}
                     nome = {'Padaria'}
                     texto = {'Minha primeira página! '}
                 />
-            
-            
+            </a>
+            <a href="https://testeclinicaportfolio.netlify.app/" target="_blank">
                 <Card 
                     imagem = {Clinica}
                     nome = {'Clínica Médica'}
                     texto = {'Site de uma clinica. '}
-            />
-            
+                />
+            </a>
         </div>
         </section>
     );
